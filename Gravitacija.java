3aaf7aea361ec286dd1d7a268867078d6765b365
mkos003 <mk12000@student.uni-lj.si>
2023-02-22 15:40:41 +0100

    Dodana funkcija za izracun gravitacijskega pospeska

--- Gravitacija.java
+++ Gravitacija.java
@@ -1,5 +1,14 @@
+import java.lang.Math;
 public class Gravitacija{
     public static void main(String[]args){
-        //funkcija, ki za podano nadmorsko višino izračuna gravitacijski pospešek
+        
+    }
+    public static double izracunNadmorskeVisine(int nadmorskaVisina){
+        double C = 6.673e-11;
+        double M = 5.98e24;
+        double r= 6.371e6;
+        double R=r+nadmorskaVisina;
+        R=Math.pow(R,2);
+        return (C*M)/R;
     }
 }
