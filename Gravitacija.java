f851a650532121739dbc8a8ab3d865ae4116a859
mkos003 <mk12000@student.uni-lj.si>
2023-02-22 18:30:05 +0100

    dodan bralnik in klic metod

--- Gravitacija.java
+++ Gravitacija.java
@@ -1,6 +1,12 @@
 import java.lang.Math;
+import java.util.*;
 public class Gravitacija{
     public static void main(String[]args){
+        Scanner bralnik=new Scanner(System.in);
+        System.out.println("Vnesite visino:");
+        int visina=bralnik.nextInt();
+        izpis();
+
 
     }
     public static void izpis(int nadmorskaVisina){
